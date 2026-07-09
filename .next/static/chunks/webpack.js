2ebd296d446ef73359db336cd054155500e69a0a
ja.ni.sha <janishachinni@gmail.com>
2026-07-09 09:45:44 +0530

    ci: add GitHub Actions workflow

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "e107440d6460ac96"; }
+/******/ 		__webpack_require__.h = function() { return "a0b49d643b4b44f6"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.3d6fdabf960d7797.hot-update.js
+++ .next/static/webpack/webpack.3d6fdabf960d7797.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "e107440d6460ac96"; }
+/******/ 	__webpack_require__.h = function() { return "a0b49d643b4b44f6"; }
 /******/ }();
 /******/ 
 /******/ }
